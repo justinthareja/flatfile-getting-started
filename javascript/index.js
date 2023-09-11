df03baf969471af70e1237cc6a127985d9a55c5c
Justin <justin.thareja@gmail.com>
2023-09-11 12:12:45 -0700

    add notes from tutorial

--- javascript/index.js
+++ javascript/index.js
@@ -7,94 +7,93 @@ import api from "@flatfile/api";
 import axios from "axios";
 
 export default function flatfileEventListener(listener) {
-  /**
-   * Part 1 example
-   */
   listener.on("**", ({ topic }) => {
     console.log(`Received event: ${topic}`);
   });
-  /**
-   * Part 3 example
-   */
+
+  // validate and transform contacts
   listener.use(
     recordHook("contacts", (record) => {
-      const value = record.get("firstName");
-      if (typeof value === "string") {
-        record.set("firstName", value.toLowerCase());
+      const firstName = record.get("firstName");
+      if (typeof firstName === "string") {
+        // transform a record
+        record.set("firstName", firstName.toLowerCase());
       }
 
       const email = record.get("email");
       const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!validEmailAddress.test(email)) {
-        console.log("Invalid email address");
-        record.addError("email", "Invalid email address");
+
+      if (email !== null && !validEmailAddress.test(email)) {
+        console.log("Invalid Email Address");
+        // error out a record
+        record.addError("email", "Invalid Email Address");
       }
 
       return record;
     })
   );
-  /**
-   * Part 3 example
-   */
+
+  // submit action
   listener.filter({ job: "workbook:submitAction" }, (configure) => {
-    configure.on("job:ready", async (event) => {
-      const { jobId, workbookId } = event.context;
+    configure.on(
+      "job:ready",
+      async ({ context: { jobId, workbookId }, payload }) => {
+        const { data: sheets } = await api.sheets.list({ workbookId });
 
-      const sheets = await api.sheets.list({ workbookId });
+        const records = {};
+        for (const [index, element] of sheets.entries()) {
+          records[`Sheet[${index}]`] = await api.records.get(element.id);
+        }
 
-      const records = {};
-      for (const [index, element] of sheets.data.entries()) {
-        records[`Sheet[${index}]`] = await api.records.get(element.id);
-      }
+        try {
+          await api.jobs.ack(jobId, {
+            info: "Starting job to submit action to webhook.site",
+            progress: 10,
+          });
 
-      try {
-        await api.jobs.ack(jobId, {
-          info: "Starting job to submit action to webhook.site",
-          progress: 10,
-        });
+          const webhookReceiver = process.env.WEBHOOK_SITE_URL;
 
-        const webhookReceiver =
-          process.env.WEBHOOK_SITE_URL ||
-          "https://webhook.site/c83648d4-bf0c-4bb1-acb7-9c170dad4388"; //update this
-
-        const response = await axios.post(
-          webhookReceiver,
-          {
-            ...event.payload,
-            method: "axios",
-            sheets,
-            records,
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
+          // Here is where to submit to sundial_api
+          // TODO: discuss interface with Alex
+          const response = await axios.post(
+            webhookReceiver,
+            {
+              ...payload,
+              method: "axios",
+              sheets,
+              records,
             },
-          }
-        );
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
 
-        if (response.status === 200) {
-          await api.jobs.complete(jobId, {
+          if (response.status === 200) {
+            await api.jobs.complete(jobId, {
+              outcome: {
+                message:
+                  "Data was successfully submitted to webhook.site. Go check it out at " +
+                  webhookReceiver +
+                  ".",
+              },
+            });
+          } else {
+            throw new Error("Failed to submit data to webhook.site");
+          }
+        } catch (error) {
+          console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
+
+          await api.jobs.fail(jobId, {
             outcome: {
               message:
-                "Data was successfully submitted to webhook.site. Go check it out at " +
-                webhookReceiver +
-                ".",
+                "This job failed probably because it couldn't find the webhook.site URL.",
             },
           });
-        } else {
-          throw new Error("Failed to submit data to webhook.site");
         }
-      } catch (error) {
-        console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
-
-        await api.jobs.fail(jobId, {
-          outcome: {
-            message:
-              "This job failed probably because it couldn't find the webhook.site URL.",
-          },
-        });
       }
-    });
+    );
   });
 }
 
